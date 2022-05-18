2a648fc701d6058a27b2e6e1f45b5055835529ed
T1vz <tima.bevz@gmail.com>
2022-05-18 12:40:03 +0300

    MapPage init

--- client/src/pages/Game/GamePage.js
+++ client/src/pages/Game/GamePage.js
@@ -1,7 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { AuthContext } from '../../context/AuthContext'
+import React, {useContext, useEffect, useState} from 'react'
+import {AuthContext} from '../../context/AuthContext'
+import {YMaps, Map, Placemark} from 'react-yandex-maps'
 
 export const GamePage = (props) => {
+
+    const [lat, setLat] = useState('err-latitude')
+    const [lon, setLon] = useState('err-longitude')
+
+    const getMyLocation = () => {
+        const location = window.navigator && window.navigator.geolocation
+
+        if (location) {
+            location.getCurrentPosition((position) => {
+                setLat(position.coords.latitude)
+                setLon(position.coords.longitude)
+            }, (error) => {
+                setLat('err-latitude')
+                setLon('err-longitude')
+            })
+        }
+
+    }
 
     const [rooms, setRooms] = useState([])
     console.log(props)
@@ -22,9 +41,9 @@ export const GamePage = (props) => {
     //             console.log('data', message)
     //         }
     //     }
-        
+
     // }, [props.client])
-    if(props.gameState.isConnected){
+    if (props.gameState.isConnected) {
         console.log('qq2')
         // props.gameState.client.onopen = () => {
         //     console.log('WebScoket client connected')
@@ -33,27 +52,45 @@ export const GamePage = (props) => {
             console.log('data', message)
             const mes = JSON.parse(message.data)
             console.log('parse', mes)
-            if (mes.type === 'rooms'){
+            if (mes.type === 'rooms') {
                 setRooms([...mes.text])
             }
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         console.log('rooms', rooms)
     }, [rooms])
-    
+
+    useEffect(() => {
+        getMyLocation()
+    }, [])
+
     return (
         <div className="row">
             <div className="center">
-                <p>Coming soon...</p>
+                <YMaps>
+                    <div>
+                        {lat}|{lon}
+                        Найди компанию для прогулки
+                        <Map height={"600px"} width={"1300px"}
+                             defaultState={lat == 'err-latitude' || lon == 'err-longitude' ? {
+                                 center: [59.8695133, 30.3080524],
+                                 zoom: 9
+                             } : {center: [lat, lon], zoom: 9}}>
+                            <Placemark
+                                geometry={lat == 'err-latitude' || lon == 'err-longitude' ? [59.8695133, 30.3080524] : [lat, lon]}/>
+                        </Map>
+
+                    </div>
+                </YMaps>
                 {/*{rooms.map((elem, index)=>{*/}
                 {/*    return <a>{(index+1.).toString() + '. ' + elem.users.map((elem2)=> elem2.username ).join()}</a>*/}
                 {/*})}*/}
                 {/*<div>*/}
                 {/*    <button onClick={CreateGameHandler}>Create game</button>*/}
                 {/*</div>*/}
-                
-            </div>  
+
+            </div>
         </div>
     )
 }
